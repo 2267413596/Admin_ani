087a772129b6e71759b945f153a629fd80cb37be
2267413596 <2267413596@qq.com>
2023-04-25 00:59:35 +0800

    finish comment

--- src/views/admin/CommentView.vue
+++ src/views/admin/CommentView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="root" >
+  <div id="root">
     <el-row>
       <el-col :span="4" :offset="1">
         <span style="margin-left: 50px">待审核评论：</span>
@@ -7,28 +7,44 @@
           v-infinite-scroll="load"
           class="infinite-list"
           style="overflow: auto"
+          v-if="!this.empty"
         >
-          <li v-for="i in count" :key="i" class="infinite-list-item" @click="check(i)">{{ tableData.list[i-1].id }}</li>
+          <li
+            v-for="i in count"
+            :key="i"
+            class="infinite-list-item"
+            @click="handelClick(i)"
+          >
+            {{ tableData.list[i - 1].username }}
+          </li>
         </ul>
+        <div v-if="this.empty" style="margin-left: 50px; top: 30px">
+          无待审核内容
+        </div>
       </el-col>
       <el-col :span="12" :offset="1" v-if="!this.empty">
         <div class="basic-box" id="infomation" style="font-size: 8px">
           <p style="text-align: center">基本信息</p>
           <el-row>
-            <el-col :span="8">
-              <span>帖子id: {{ this.tweetId }}</span>
+            <el-col :span="4">
+              <span>帖子id: {{ this.idC }}</span>
             </el-col>
-            <el-col :span="8">
+            <el-col :span="4">
               <span>用户：{{ this.userName }}</span>
             </el-col>
-            <el-col :span="8">
-              <span>日期：{{ this.date }}</span>
+            <el-col :span="16">
+              <span>时间: {{ this.date }}</span>
             </el-col>
           </el-row>
         </div>
         <div class="basic-box" id="comment">
           <p style="text-align: center">评论内容</p>
           <span>{{ this.content }}</span>
+        </div>
+      </el-col>
+      <el-col :span="12" :offset="1" v-if="this.empty">
+        <div style="margin-top: 20vh; margin-left: 20vw">
+          <img src="/relex.jpg" alt="Header" height="200" />
         </div>
       </el-col>
     </el-row>
@@ -42,9 +58,9 @@
         <el-col :span="22">
           <el-button type="danger" @click="fail">驳回</el-button>
         </el-col>
-          <el-col :span="2">
-            <el-button type="success" @click="pass">通过</el-button>
-          </el-col>
+        <el-col :span="2">
+          <el-button type="success" @click="pass">通过</el-button>
+        </el-col>
       </el-row>
     </div>
   </div>
@@ -89,36 +105,41 @@
 </style>
 
 <script>
-import { defineComponent, onMounted, ref , reactive} from "vue";
+import { defineComponent, onMounted, ref, reactive } from "vue";
 import Axios from "axios";
 import { useCookies } from "vue3-cookies";
 import { method } from "lodash";
+import { useRouter } from "vue-router";
+import { ElMessage } from 'element-plus'
 
+
+const { cookies } = useCookies();
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: cookies.get("myCookie"),
+};
 export default defineComponent({
   setup() {
-    const { cookies } = useCookies();
     const empty = ref(true);
     const account = ref("");
     var token = ref("aa");
     const password = ref("");
+    const router = useRouter();
     var config = {
       headers: {
         Authorization: "",
       },
     };
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: cookies.get("myCookie"),
-    };
     const count = ref(0);
     const max = ref(0);
     const page = ref(0);
     let tableData = reactive({ list: [] });
-    const reason = ref('')
-    const tweetId = ref(0)
-    const date = ref('')
-    const userName = ref('')
-    const content = ref('')
+    const reason = ref("");
+    const tweetId = ref(0);
+    const date = ref("");
+    const userName = ref("");
+    const content = ref("");
+    const idC = ref(0);
     Axios.post(
       "/api/admin/comment/get/",
       {
@@ -140,11 +161,11 @@ export default defineComponent({
         max.value = response.data.body.sumNum;
         if (count.value > 0) {
           empty.value = false;
-          tweetId.value = tableData.list[0].tweetId
-          id.value = tableData.list[0].id
-          // userName.value = tableData.list[0].userName
-          // content.value = tableData.list[0].content
-          date.value = tableData.list[0].time
+          tweetId.value = tableData.list[0].tweetId;
+          idC.value = tableData.list[0].id;
+          userName.value = tableData.list[0].username;
+          content.value = tableData.list[0].content;
+          date.value = tableData.list[0].time;
         }
       })
       .catch((response) => {
@@ -161,12 +182,13 @@ export default defineComponent({
       tableData,
       page,
       max,
-      headers,
       reason,
       tweetId,
       date,
       userName,
-      content
+      content,
+      router,
+      idC,
     };
   },
   methods: {
@@ -180,7 +202,7 @@ export default defineComponent({
           pageNum: 20,
           page: this.page,
         },
-        { headers }
+        { headers  }
       )
         .then((response) => {
           console.log(response);
@@ -206,12 +228,60 @@ export default defineComponent({
         this.count = this.max;
       }
     },
-    fail() {
-
+    handelClick(i) {
+      this.idC = this.tableData.list[i - 1].id;
+      this.userName = this.tableData.list[i - 1].username;
+      this.date = this.tableData.list[i - 1].time;
+      this.content = this.tableData.list[i - 1].content;
     },
     pass() {
-
-    }
+      Axios.post(
+        "/api/admin/comment/censor/",
+        {
+          commentId: this.idC,
+          operate: 0,
+          reason: "",
+        },
+        { headers }
+      )
+        .then((response) => {
+          if(response.data.code != 0) {
+            ElMessage(response.data.message);
+          } else {
+            ElMessage('Success!');
+          }
+          setTimeout(1000);
+          this.router.go(0)
+        })
+        .catch((response) => {
+          ElMessage("网络错误");
+          console.log(response);
+        });
+    },
+    fail() {
+      Axios.post(
+        "/api/admin/comment/censor/",
+        {
+          commentId: this.idC,
+          operate: 1,
+          reason: this.reason,
+        },
+        { headers }
+      )
+        .then((response) => {
+          if(response.data.code != 0) {
+            ElMessage(response.data.message);
+          } else {
+            ElMessage('Success!');
+          }
+          setTimeout(1000);
+          this.router.go(0)
+        })
+        .catch((response) => {
+          ElMessage("网络错误");
+          console.log(response);
+        });
+    },
   },
 });
 </script>
